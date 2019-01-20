4526da4ca799cca0ac607d26528ef95b94b96a46
William Saffran <willh.saffran@gmail.com>
2019-01-20 22:40:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,9 +15,21 @@ const codes = [
 
 function init() {
   // your code here
-  const alphabet = ['a', 'b', 'c']
-  
-  let index = 0
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
+
+     if (key === code[index]) {
+      index++;
+
+       if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }, false);
   
   
   
